dc75afeb6fc78c4306ef4fe288c8ad288dd50543
Desmond Chin <chin.sy.des@gmail.com>
2023-10-07 16:12:22 +0900

    refactor player.gd

--- player.gd
+++ player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 180.0
+const JUMP_VELOCITY = -250.0
 const COYOTE_FRAMES = 6
 const ACCELERATION = 0.25
 const FRICTION = 0.1
@@ -10,8 +10,8 @@ const FRICTION = 0.1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
-@onready var coyote_timer : Timer = $CoyoteTimer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $CoyoteTimer
 
 var last_floor = false
 var is_in_coyote = false
@@ -21,44 +21,50 @@ func _ready():
 	coyote_timer.wait_time = COYOTE_FRAMES / 60.0
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	apply_gravity(delta)
+	apply_coyote()
+	handle_jump()
+	var direction: float = Input.get_axis("ui_left", "ui_right")
+	handle_movement(direction)
+	apply_animation(direction)
 
+	last_floor = is_on_floor()
+	move_and_slide()
+
+func apply_coyote():
 	if is_in_coyote and is_on_floor():
 		is_in_coyote = false
 	elif not is_in_coyote and last_floor:
 		coyote_timer.start()
 		is_in_coyote = true
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_in_coyote):
-		velocity.y = JUMP_VELOCITY
-		is_in_coyote = false
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+func handle_movement(direction: float):
 	if direction:
 		velocity.x = lerp(velocity.x, direction * SPEED, ACCELERATION)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, FRICTION)
-		
+
+func handle_jump():
+	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or is_in_coyote):
+		velocity.y = JUMP_VELOCITY
+		is_in_coyote = false
+
+func apply_gravity(delta: float):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+func apply_animation(direction: float):
 	if not is_on_floor():
 		animated_sprite.play("jump")
 	elif direction:
 		animated_sprite.play("run")
 	elif is_on_floor():
 		animated_sprite.play("idle")
-		
+
 	if velocity.x > 0:
 		animated_sprite.flip_h = false
 	elif velocity.x < 0:
 		animated_sprite.flip_h = true
-		
-	last_floor = is_on_floor()
-	move_and_slide()
-
 
 func _on_coyote_timer_timeout():
 	is_in_coyote = false
