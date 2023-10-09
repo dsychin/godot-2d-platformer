f8d9c0cd98f520d6365222943df5676d61ab9ea0
Desmond Chin <chin.sy.des@gmail.com>
2023-10-09 19:53:18 +0900

    add test for coyote jump

--- test/integration/test_player.gd
+++ test/integration/test_player.gd
@@ -1,55 +1,86 @@
 extends GutTest
 
-var Scene: PackedScene = load("res://src/main.tscn")
-var _scene: Node = null
-var _player: CharacterBody2D = null
-var _sender = InputSender.new(Input)
+class TestFlatGround:
+	extends GutTest
 
-func before_each():
-	_scene = add_child_autofree(Scene.instantiate())
-	_player = _scene.get_node("Player")
-	await wait_frames(2)
+	var Scene: PackedScene = load("res://src/main.tscn")
+	var _scene: Node = null
+	var _player: CharacterBody2D = null
+	var _sender = InputSender.new(Input)
 
-func after_each():
-	_sender.release_all()
-	_sender.clear()
+	func before_each():
+		_scene = add_child_autofree(Scene.instantiate())
+		_player = _scene.get_node("Player")
+		await wait_frames(2)
 
-func test_player_should_be_on_floor():
-	assert_true(_player.is_on_floor())
+	func after_each():
+		_sender.release_all()
+		_sender.clear()
 
-func test_player_moves_right():
-	var initialPosition = _player.global_position
-	await _sender.action_down("ui_right").hold_for(.2).idle
+	func test_player_should_be_on_floor():
+		assert_true(_player.is_on_floor())
 
-	assert_gt(_player.global_position.x, initialPosition.x)
+	func test_player_moves_right():
+		var initialPosition = _player.global_position
+		await _sender.action_down("ui_right").hold_for(.2).idle
 
-func test_player_moves_left():
-	var initialPosition = _player.global_position
-	await _sender.action_down("ui_left").hold_for(.2).idle
+		assert_gt(_player.global_position.x, initialPosition.x)
 
-	assert_lt(_player.global_position.x, initialPosition.x)
+	func test_player_moves_left():
+		var initialPosition = _player.global_position
+		await _sender.action_down("ui_left").hold_for(.2).idle
 
-func test_player_jumps():
-	await _sender.action_down("ui_accept").hold_for(.1).idle
+		assert_lt(_player.global_position.x, initialPosition.x)
 
-	assert_lt(_player.velocity.y, 0.0)
+	func test_player_jumps():
+		await _sender.action_down("ui_accept").hold_for(.1).idle
 
-func test_player_should_have_idle_animation():
-	await wait_seconds(.1)
+		assert_lt(_player.velocity.y, 0.0)
 
-	assert_eq("idle", _player.get_node("AnimatedSprite2D").get_animation())
+	func test_player_should_have_idle_animation():
+		await wait_seconds(.1)
 
-func test_player_should_have_run_animation():
-	await _sender.action_down("ui_right").hold_for(.2).idle
+		assert_eq("idle", _player.get_node("AnimatedSprite2D").get_animation())
 
-	assert_eq("run", _player.get_node("AnimatedSprite2D").get_animation())
+	func test_player_should_have_run_animation():
+		await _sender.action_down("ui_right").hold_for(.2).idle
 
-func test_player_should_have_jump_animation():
-	await _sender.action_down("ui_accept").hold_for(.2).idle
+		assert_eq("run", _player.get_node("AnimatedSprite2D").get_animation())
 
-	assert_eq("jump", _player.get_node("AnimatedSprite2D").get_animation())
+	func test_player_should_have_jump_animation():
+		await _sender.action_down("ui_accept").hold_for(.2).idle
 
-func test_player_should_face_left():
-	await _sender.action_down("ui_left").hold_for(.2).idle
+		assert_eq("jump", _player.get_node("AnimatedSprite2D").get_animation())
 
-	assert_true(_player.get_node("AnimatedSprite2D").flip_h)
+	func test_player_should_face_left():
+		await _sender.action_down("ui_left").hold_for(.2).idle
+
+		assert_true(_player.get_node("AnimatedSprite2D").flip_h)
+
+class TestPlatformEdge:
+	extends GutTest
+
+	var Scene: PackedScene = load("res://test/resource/platform_edge.tscn")
+	var _scene: Node = null
+	var _player: CharacterBody2D = null
+	var _sender = InputSender.new(Input)
+
+	func before_each():
+		_scene = add_child_autofree(Scene.instantiate())
+		_player = _scene.get_node("Player")
+		await wait_frames(5)
+
+	func after_each():
+		_sender.release_all()
+		_sender.clear()
+
+	func test_player_should_be_on_floor():
+		assert_true(_player.is_on_floor())
+
+	func test_player_should_be_able_to_jump_after_leaving_floor():
+		_player.get_node("CoyoteTimer").wait_time = 1.0
+		await _sender.action_down("ui_right").hold_for(.3).idle
+		assert_false(_player.is_on_floor(), "player should have left the platform")
+		await _sender.action_down("ui_accept").wait_frames(1).idle
+
+		assert_lt(_player.velocity.y, 0.0, "player should have jumped")
